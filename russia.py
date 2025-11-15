2dc99117a82ea43b746a284a7e13e416771deec6
ZIDAN <zidankhan5761@gmail.com>
2025-11-15 19:18:52 +0530

    Update russia.py

--- russia.py
+++ russia.py
@@ -1,24 +1,20 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-# Create figure with grey background
 fig, ax = plt.subplots(figsize=(6, 4))
-fig.patch.set_facecolor("lightgrey")   # background of figure
-ax.set_facecolor("lightgrey")          # background of axes
+fig.patch.set_facecolor("lightgrey")   
+ax.set_facecolor("lightgrey")         
 
-# Draw white stripe
 ax.add_patch(patches.Rectangle((0, 2/3), 1, 1/3, color="white"))
 
-# Draw blue stripe
 ax.add_patch(patches.Rectangle((0, 1/3), 1, 1/3, color="blue"))
 
-# Draw red stripe
 ax.add_patch(patches.Rectangle((0, 0), 1, 1/3, color="red"))
 
-# Formatting
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 ax.set_aspect('equal')
 ax.axis('off')
 
 plt.show()
+
